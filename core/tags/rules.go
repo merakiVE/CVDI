63011b58d86dfb0b38644df2158ea1a5c8ce6448
Israel Lugo <hostelixisrael@gmail.com>
2017-07-21 17:32:15 -0400

    feat(CVDI): add new rule on_create and logic

--- core/tags/rules.go
+++ core/tags/rules.go
@@ -1,23 +1,29 @@
 package tags
 
 import (
-	"fmt"
 	"github.com/fatih/structs"
 	"reflect"
 	"time"
+	//"golang.org/x/crypto/bcrypt"
+
 )
 
 type ModelParam struct {
-	field *structs.Field
-	param string
+	field      *structs.Field
+	nameAction string
+	params     []string
 }
 
 func (this ModelParam) FieldName() (string) {
 	return this.field.Name()
 }
 
-func (this ModelParam) GetParam() (string) {
-	return this.param
+func (this ModelParam) GetParams() ([]string) {
+	return this.params
+}
+
+func (this ModelParam) GetAction() (string) {
+	return this.nameAction
 }
 
 func (this ModelParam) GetField() (*structs.Field) {
@@ -26,13 +32,15 @@ func (this ModelParam) GetField() (*structs.Field) {
 
 type FieldParam interface {
 	FieldName() string
-	GetParam() string
+	GetParams() []string
+	GetAction() string
 	GetField() *structs.Field
 }
 
 var (
 	defaultTagsRules = map[string]HandleTag{
-		"default": RuleDefault,
+		"default":   RuleDefault,
+		"on_create": RuleOnCreate,
 	}
 
 	reservedTags = []string{
@@ -43,12 +51,44 @@ var (
 
 func RuleDefault(_model FieldParam) () {
 
-	if _model.GetParam() == "auto_now" {
+	if _model.GetAction() == "auto_now" {
 		if _model.GetField().Kind() == reflect.ValueOf(time.Time{}).Kind() {
 
 			_model.GetField().Set(time.Now())
 		}
 	}
+}
 
-	fmt.Println("Handle default " + _model.GetParam())
+func RuleOnCreate(f FieldParam) () {
+
+	switch f.GetAction() {
+
+		case "execute" : {
+
+			// Testing function
+		}
+
+		case "set" : {
+
+			params := f.GetParams()
+
+			if params[0] == "auto_now" {
+
+				if f.GetField().Kind() == reflect.ValueOf(time.Time{}).Kind() {
+
+					f.GetField().Set(time.Now())
+				}
+			}
+		}
+	}
+}
+
+func RuleMakePassword(f FieldParam) () {
+	if f.GetAction() == "make_password" {
+
+		if f.GetField().Kind() == reflect.String {
+			//bytes, err := bcrypt.GenerateFromPassword([]byte(f.GetField().Value()), 14)
+			//f.GetField().Set(string(bytes))
+		}
+	}
 }
