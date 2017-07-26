5bdc3b878d6b902004e2ffeeaed2e96ad66e2657
Israel Lugo <hostelixisrael@gmail.com>
2017-07-26 11:58:55 -0400

    feat(CVDI): add new method createtoken in package auth

--- core/auth/auth.go
+++ core/auth/auth.go
@@ -1,1 +1,26 @@
 package auth
+
+import (
+	"github.com/dgrijalva/jwt-go"
+)
+
+func CreateTokenJWT(_data map[string]interface{}, _secretKey interface{}) ([]byte) {
+
+	/* Create the token */
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	/* Create a map to store our claims */
+	claims := token.Claims.(jwt.MapClaims)
+
+	/* Set token claims */
+	for _key, _value := range _data {
+		claims[_key] = _value
+	}
+	//claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+
+	/* Sign the token with our secret */
+	tokenString, _ := token.SignedString(_secretKey)
+
+	/* Finally, write the token to the browser window */
+	return []byte(tokenString)
+}
