21cf5813ffad5f16f2ea7ea65abc2ca3c02ddafc
Israel Lugo <hostelixisrael@gmail.com>
2017-07-28 17:07:17 -0400

    feat(CVDI): add function read keys in package utils

--- core/utils/utils.go
+++ core/utils/utils.go
@@ -1,4 +1,18 @@
 package utils
 
+import (
+	"io/ioutil"
+)
 
+const (
+	PRIVATE_KEY_PATH = "keys/private.pem"
+	PUBLIC_KEY_PATH  = "keys/public.pem"
+)
 
+func ReadSecrectKey() ([]byte, error) {
+	return ioutil.ReadFile(PRIVATE_KEY_PATH)
+}
+
+func ReadPublicKey() ([]byte, error) {
+	return ioutil.ReadFile(PUBLIC_KEY_PATH)
+}
