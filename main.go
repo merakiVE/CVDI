4d1ae6482589242ef702f9d4d4dc543be8b8234f
Israel Lugo <hostelixisrael@gmail.com>
2017-07-28 17:24:35 -0400

    feat(CVDI): refactor main.go

--- main.go
+++ main.go
@@ -1,27 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
 
 	"github.com/kataras/iris"
-	"github.com/kataras/iris/context"
 	"github.com/kataras/iris/middleware/logger"
 
-	arangoDB "github.com/hostelix/aranGO"
-
-	"github.com/merakiVE/CVDI/core/db"
-	"github.com/merakiVE/CVDI/core/types"
-	"github.com/merakiVE/CVDI/core/auth"
-	"github.com/merakiVE/CVDI/src/models"
+	"github.com/merakiVE/CVDI/src/controllers"
+	"github.com/merakiVE/CVDI/core/utils"
 )
 
 const (
 	PORT_SERVER = ":8101"
-
-	PRIVATE_KEY_PATH = "keys/private.pem"
-	PUBLIC_KEY_PATH  = "keys/public.pem"
 )
 
 var (
@@ -31,13 +21,13 @@ var (
 func initKeys() {
 	var err error
 
-	SecrectKey, err = ioutil.ReadFile(PRIVATE_KEY_PATH)
+	SecrectKey, err = utils.ReadSecrectKey()
 	if err != nil {
 		log.Fatal("Error reading private key")
 		return
 	}
 
-	PublicKey, err = ioutil.ReadFile(PUBLIC_KEY_PATH)
+	PublicKey, err = utils.ReadPublicKey()
 	if err != nil {
 		log.Fatal("Error reading public key")
 		return
@@ -66,161 +56,26 @@ func main() {
 
 	app.Use(APILogger)
 
+	//Init Controllers
+	cAuth := controllers.AuthController{}
+	cUser := controllers.UserController{}
+
 	routerUsers := app.Party("/users")
 	{
-		routerUsers.Get("/", getAllUsers)
-		routerUsers.Post("/create", createUser)
+		routerUsers.Get("/", cUser.List)
+		routerUsers.Post("/create", cUser.Create)
 	}
 
 	routerAdmin := app.Party("/auth")
 	{
-		routerAdmin.Post("/login", login)
+		routerAdmin.Post("/login", cAuth.Login)
+	}
+
+	routerNeuron := app.Party("/neuron")
+	{
+		routerNeuron.Get("/")
+		routerNeuron.Post("/subscription")
 	}
 
 	app.Run(iris.Addr(PORT_SERVER), iris.WithCharset("UTF-8"))
-}
-
-func login(ctx context.Context) {
-	var _form types.UserCredentials
-	var _user models.UserModel
-
-	err := ctx.ReadJSON(&_form)
-
-	if err != nil {
-
-		ctx.StatusCode(iris.StatusBadRequest)
-		ctx.JSON(types.ResponseAPI{
-			Message: "Invalid data user",
-			Data:    nil,
-			Errors:  nil,
-		})
-		return
-	}
-
-	sq := fmt.Sprintf("FOR user IN users FILTER user.username == '%s' RETURN user", _form.Username)
-
-	q := arangoDB.NewQuery(sq)
-	cur, err := db.GetDatabase("meraki").Execute(q)
-
-	if err != nil {
-
-		ctx.StatusCode(iris.StatusInternalServerError)
-		ctx.JSON(types.ResponseAPI{
-			Message: "Fail",
-			Data:    nil,
-			Errors:  nil,
-		})
-		return
-	}
-
-	if !cur.FetchOne(&_user) {
-
-		ctx.StatusCode(iris.StatusInternalServerError)
-		ctx.JSON(types.ResponseAPI{
-			Message: "Error for get data user",
-			Data:    nil,
-			Errors:  nil,
-		})
-		return
-	}
-
-	if auth.VerifyPassword([]byte(_user.Password), []byte(_form.Password)) {
-
-		_token := auth.CreateTokenJWT(map[string]interface{}{"id": _user.Id, "key": _user.Key, "username": _user.Username }, SecrectKey)
-
-		ctx.StatusCode(iris.StatusOK)
-		ctx.JSON(types.ResponseAPI{
-			Message: "Login success",
-			Data: types.JsonObject{
-				"token": _token,
-			},
-			Errors: nil,
-		})
-		return
-
-	} else {
-
-		ctx.StatusCode(iris.StatusForbidden)
-		return
-	}
-}
-
-func getAllUsers(ctx context.Context) {
-
-	result := make([]models.UserModel, 0)
-	var err error
-
-	q := arangoDB.NewQuery("FOR i in users RETURN i")
-	cur, err := db.GetDatabase("meraki").Execute(q)
-
-	if err != nil {
-
-		ctx.StatusCode(iris.StatusInternalServerError)
-		ctx.JSON(types.ResponseAPI{
-			Message: "Fail",
-			Data:    nil,
-			Errors:  nil,
-		})
-		return
-	}
-
-	err = cur.FetchBatch(&result)
-
-	if err != nil {
-
-		ctx.StatusCode(iris.StatusInternalServerError)
-		ctx.JSON(types.ResponseAPI{
-			Message: "Fail",
-			Data:    nil,
-			Errors:  nil,
-		})
-		return
-	}
-
-	ctx.StatusCode(iris.StatusOK)
-	ctx.JSON(types.ResponseAPI{
-		Message: "Success",
-		Data:    result,
-		Errors:  nil,
-	})
-}
-
-func createUser(ctx context.Context) {
-
-	var _user models.UserModel
-
-	var err error
-
-	err = ctx.ReadJSON(&_user)
-
-	if err != nil {
-
-		ctx.StatusCode(iris.StatusInternalServerError)
-		ctx.JSON(types.ResponseAPI{
-			Message: "Invalid data user",
-			Data:    nil,
-			Errors:  nil,
-		})
-		return
-	}
-
-	succes := db.SaveModel(db.GetDatabase("meraki"), &_user)
-
-	if succes {
-		ctx.StatusCode(iris.StatusOK)
-		ctx.JSON(types.ResponseAPI{
-			Message: "User created successfully",
-			Data:    nil,
-			Errors:  nil,
-		})
-
-	} else {
-		ctx.StatusCode(iris.StatusOK)
-		ctx.JSON(types.ResponseAPI{
-			Message: "Error creating user, invalid data",
-			Data:    nil,
-			Errors:  _user.GetValidationErrors(),
-		})
-	}
-
 }
