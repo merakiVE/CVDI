b98bf9fa7f49dd1898671e056a4288bdb64649e8
Israel Lugo <hostelixisrael@gmail.com>
2017-08-01 00:01:12 -0400

    feat(CVDI): refactor function utils

--- core/utils/utils.go
+++ core/utils/utils.go
@@ -4,15 +4,6 @@ import (
 	"io/ioutil"
 )
 
-const (
-	PRIVATE_KEY_PATH = "keys/private.pem"
-	PUBLIC_KEY_PATH  = "keys/public.pem"
-)
-
-func ReadSecrectKey() ([]byte, error) {
-	return ioutil.ReadFile(PRIVATE_KEY_PATH)
-}
-
-func ReadPublicKey() ([]byte, error) {
-	return ioutil.ReadFile(PUBLIC_KEY_PATH)
+func ReadBinaryFile(_path string) ([]byte, error) {
+	return ioutil.ReadFile(_path)
 }
