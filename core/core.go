2f1ec88afd12cfd11139ee785c771f690925d305
Israel Lugo <hostelixisrael@gmail.com>
2017-08-03 12:28:26 -0400

    update(CVDI): add application to struct contextController

--- core/core.go
+++ core/core.go
@@ -1,9 +1,13 @@
 package core
 
-import "github.com/merakiVE/CVDI/core/config"
+import (
+	"github.com/merakiVE/CVDI/core/config"
+	"github.com/kataras/iris"
+)
 
 type ContextController struct {
 	Config config.Configuration
+	App    *iris.Application
 }
 
 type Controller interface {
