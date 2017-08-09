6677593cc5b2342fa993884a9c73299df7381471
Israel Lugo <hostelixisrael@gmail.com>
2017-08-09 15:23:03 -0400

    update(CVDI): add methods getmodel and replacemodel to package db

--- core/db/db.go
+++ core/db/db.go
@@ -2,10 +2,11 @@ package db
 
 import (
 	"reflect"
+	"log"
 
 	arangoDB "github.com/hostelix/aranGO"
-
 	"github.com/merakiVE/CVDI/core/config"
+	"github.com/merakiVE/CVDI/core/utils"
 )
 
 var DBHOST, DBUSER, DBPASSWORD string
@@ -70,5 +71,44 @@ func SaveModel(_db *arangoDB.Database, _model arangoDB.Modeler) (bool) {
 		return false
 	}
 
+	return true
+}
+
+func GetModel(_db *arangoDB.Database, _model arangoDB.Modeler) (bool) {
+
+	if reflect.ValueOf(_model).Kind() != reflect.Ptr {
+		panic("Check model must be a pointer")
+	}
+
+	ctx, err := arangoDB.NewContext(_db)
+
+	if err != nil {
+		log.Fatal(err)
+		panic(err)
+	}
+
+	// save model, returns map of errors or empty map
+	e := ctx.Get(_model)
+
+	// check errors, also Error is saved in Context struct
+	if len(e) >= 1 {
+		return false
+	}
+
+	return true
+}
+
+func ReplaceModel(_db *arangoDB.Database, _model arangoDB.Modeler) (bool) {
+
+	if utils.IsEmptyString(_model.GetKey()) {
+		panic("Check key is empty")
+	}
+
+	err := _db.Col(_model.GetCollection()).Replace(_model.GetKey(), _model)
+
+	if err != nil {
+		log.Fatal(err)
+		return false
+	}
 	return true
 }
