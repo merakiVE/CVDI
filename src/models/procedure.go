1189940b0716b02b4af4a88d7ee6a7f112bf1bcb
Israel Lugo <hostelixisrael@gmail.com>
2017-09-04 22:28:42 -0400

    update(CVDI/procedure): changing struct for procedure

--- src/models/procedure.go
+++ src/models/procedure.go
@@ -6,75 +6,34 @@ import (
 	"github.com/merakiVE/CVDI/core/types"
 	"github.com/merakiVE/CVDI/core/validator"
 	"github.com/merakiVE/CVDI/core/tags"
-	"sort"
 )
 
-const (
-	TYPE_NODE_START        = 1
-	TYPE_NODE_END          = 2
-	TYPE_NODE_TASK         = 3
-	TYPE_NODE_USER_TASK    = 4
-	TYPE_NODE_SERVICE_TASK = 5
-	TYPE_NODE_GATEWAY      = 6
-	TYPE_NODE_TIMER        = 7
-	TYPE_NODE_MESSAGE      = 8
-)
-
-type Node struct {
-	Type        int
-	NextNode    *Node
-	PreviusNode *Node
-	Data        interface{}
-}
-
-type TypeNode interface {
-	GetType() string
-	GetNexNode() *Node
-	GetPreviosNode() *Node
-	GetData() interface{}
-}
-
-//Structs for BPMN
 type Lane struct {
-	Name       string        `json:"name"`
-	InPool     bool          `json:"in_pool"`
-	NamePool   string        `json:"name_pool"`
-	Activities []Activity    `json:"activities,omitempty" validate:"required"`
-}
-
-type Bpmn struct {
-	Lanes []Lane `json:"lanes"`
-}
-
-func (this Bpmn) GetSequenceActivities() ([]Activity) {
-	activities_tmp := make([]Activity, 0)
-
-	for _, lane := range this.Lanes {
-		for _, acti := range lane.Activities {
-			activities_tmp = append(activities_tmp, acti)
-		}
-	}
-
-	sort.Slice(activities_tmp, func(i, j int) bool {
-		return activities_tmp[i].Sequence < activities_tmp[j].Sequence
-	})
-
-	return activities_tmp
+	ID            string `json:"id"`
+	Name          string `json:"name"`
+	PoolRef       string `json:"pool_ref"`
+	ActivitiesRef []string `json:"activities_ref"`
 }
 
 type Activity struct {
-	Name      string `json:"name"`
-	NeuronKey string `json:"neuron_key"`
-	ActionID  string `json:"action_id"`
-	Sequence  int32    `json:"sequence"`
-	InputData map[string]interface{} `json:"input_data"`
+	ID         string `json:"id"`
+	Name       string `json:"name"`
+	NeuronKey  string `json:"neuron_key"`
+	ActionID   string `json:"action_id"`
+	Sequence   int32  `json:"sequence"`
+	Type       string `json:"type"` //manually - automatic
+	InputData  map[string]interface{} `json:"input_data"`
+	OutputData map[string]interface{} `json:"output_data"`
 }
 
 type ProcedureModel struct {
 	arangoDB.Document
 
-	ID    string        `json:"id" validate:"required" on_create:"set,auto_uuid"`
-	Owner string        `json:"owner,omitempty" validate:"required"`
+	ID         string        `json:"id" validate:"required" on_create:"set,auto_uuid"`
+	Owner      string        `json:"owner,omitempty" validate:"required"`
+	Pool       string        `json:"pool"`
+	Lanes      []Lane        `json:"lanes"`
+	Activities []Activity    `json:"activities,omitempty" validate:"required"`
 
 	types.Timestamps
 	ErrorsValidation []map[string]string `json:"errors_validation,omitempty"`
